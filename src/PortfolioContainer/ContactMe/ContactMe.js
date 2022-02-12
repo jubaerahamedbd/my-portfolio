06eec57a68bfd3a2461f2efca32b2ddc5305749f
Jubaer Ahamed <jahamed171200@bscse.uiu.ac.bd>
2022-02-12 23:37:44 +0600

    Contact Us

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Typical from 'react-typical'
 import axios from 'axios';
+import emailjs from '@emailjs/browser';
 import { toast } from "react-toastify";
 import imgBack from '../../images/mailz.jpeg'
 import load1 from '../../images/load2.gif'
@@ -13,6 +14,7 @@ import './ContactMe.css'
 
 
 const ContactMe = (props) => {
+    const form = useRef()
     let fadeInScreenHandler = (screen) => {
         if (screen.faceScreen !== props.id)
             return
@@ -29,7 +31,6 @@ const ContactMe = (props) => {
 
     const handleName = e => {
         setName(e.target.value);
-        console.log(name);
     }
 
     const handleEmail = e => {
@@ -42,100 +43,104 @@ const ContactMe = (props) => {
     const handleMessage = e => {
         setMessage(e.target.value);
     }
-    console.log(phone);
-    const submitForm =async e => {
-        e.preventDefault();
-        try {
-            let data = {
-                name,
-                email,
-                message,
-            };
-            setBool(true);
-            const res = await axios.post(`/contact`, data);
-            if (name.length === 0 || email.length === 0 || message.length === 0) {
-                setBanner(res.data.msg);
-                toast.error(res.data.msg);
-                setBool(false);
-            } else if (res.status === 200) {
-                setBanner(res.data.msg);
-                toast.success(res.data.msg);
-                setBool(false);
 
-                setName("");
-                setEmail("");
-                setMessage("");
-            }
-        } catch (error) {
-            console.log(error);
-        }
+    const sendEmail = e => {
+        e.preventDefault();
+        // let data = {
+        //     name,
+        //     email,
+        //     phone,
+        //     message,
+        // };
+        setBool(true)
+        setBanner("");
+        emailjs.sendForm('service_s4cdqvz', 'template_o4d7wvl', e.target, 'user_XTKCAJroxhgUGWB4mDuIo')
+            .then((result) => {
+                console.log(result.text);
+                setBool(false);
+                setBanner("Successfully Sent");
+                e.target.reset();
+                setName("")
+                setEmail("")
+                setPhone("")
+                setMessage("")
+            }, (error) => {
+                console.log(error.text);
+                setBanner("Please Check Input Field");
+            });
+
     };
     return (
-        <div className='main-container' id={props.id || ''}>
-            <ScreenHeading
-                title={"Contact Me"}
-                subHeading={"Lets Keep In Touch"}
-            />
-            <div className='central-form'>
-                <div className='col'>
-                    <h2>
-                        {""}
-                        <Typical
-                            loop={Infinity}
-                            steps={
-                                [
-                                    "Get In Touch 📧", 1000,
-                                ]
-                            }
-                        />
-                    </h2>
-                    <a href='https://www.facebook.com/jubaerahamedbd/'>
-                        <i className='fa fa-facebook-square'></i>
-                    </a>
-                    <a href='#'>
-                        <i className='fa fa-google-plus-square'></i>
-                    </a>
-                    <a href='https://www.instagram.com/jubaerahamedbd'>
-                        <i className='fa fa-instagram'></i>
-                    </a>
-                    <a href='https://twitter.com/jubaerahamedbd'>
-                        <i className='fa fa-twitter'></i>
-                    </a>
-                </div>
-                <div className='back-form'>
-                    <div className='img-back'>
-                        <h4>Send Your Email Here!</h4>
-                        <img src={imgBack} alt='not found' />
+        <div className='outter-container'>
+            <div className='main-container' id={props.id || ''}>
+                <ScreenHeading
+                    title={"Contact Me"}
+                    subHeading={"Lets Keep In Touch"}
+                />
+                <div className='central-form'>
+                    <div className='col'>
+                        <h2>
+                            {""}
+                            <Typical
+                                loop={Infinity}
+                                steps={
+                                    [
+                                        "Get In Touch 📧", 1000,
+                                    ]
+                                }
+                            />
+                        </h2>
+                        <a href='https://www.facebook.com/jubaerahamedbd/'>
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <a href='#'>
+                            <i className='fa fa-google-plus-square'></i>
+                        </a>
+                        <a href='https://www.instagram.com/jubaerahamedbd'>
+                            <i className='fa fa-instagram'></i>
+                        </a>
+                        <a href='https://twitter.com/jubaerahamedbd'>
+                            <i className='fa fa-twitter'></i>
+                        </a>
                     </div>
-                    <form onSubmit={submitForm}>
-                        <p>{banner}</p>
-                        <label htmlFor='name'>Name</label>
-                        <input type='text' onChange={handleName} value={name} />
-
-                        <label htmlFor='email'>Email</label>
-                        <input type='email' onChange={handleEmail} value={email} />
-
-                        <label htmlFor='phone'>Phone No</label>
-                        <input type='number' onChange={handlePhone} value={phone} />
-
-                        <label htmlFor='message'>Message</label>
-                        <textarea type='text' onChange={handleMessage} value={message} />
-                        <div className='send-btn'>
-                            <button type='submit'>
-                                Send <i className='fa fa-paper-plane'></i>
-                                {bool ? (
-                                    <b className="load">
-                                        <img src={load1} alt="image not responding" />
-                                    </b>
-                                ) : (
-                                    ""
-                                )}
-                            </button>
+                    <div className='back-form'>
+                        <div className='img-back'>
+                            <h4>Send Your Email Here!</h4>
+                            <img src={imgBack} alt='not found' />
                         </div>
-                    </form>
-                </div>
-            </div>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <p>{banner}</p>
+                            <label htmlFor='name'>Name</label>
+                            <input type='text' onChange={handleName} name="name" value={name} required />
 
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' onChange={handleEmail} name="email" value={email} required />
+
+                            <label htmlFor='phone'>Phone No</label>
+                            <input type='number' onChange={handlePhone} name="phone" value={phone} />
+
+                            <label htmlFor='message'>Message</label>
+                            <textarea type='text' onChange={handleMessage} name="message" value={message} required />
+                            <div className='send-btn'>
+                                <button type='submit' value={"send"}>
+                                    Send <i className='fa fa-paper-plane'></i>
+                                    {bool ? (
+                                        <b className="load">
+                                            <img src={load1} alt="image not responding" />
+                                        </b>
+                                    ) : (
+                                        ""
+                                    )}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+            <div className='footer'>
+                <p>©️jubaerahamed.me 2022</p>
+            </div>
         </div>
     );
 };
